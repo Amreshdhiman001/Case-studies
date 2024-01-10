909137bae7eaebd3c5dbaabdd863b4209e8b93ad
Amreshdhiman001 <154113809+Amreshdhiman001@users.noreply.github.com>
2024-01-10 17:33:53 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded, function(){
+                          alert("Welcom to My Prtfolio!")
+                          // You can add more javascript code for interactivity or other feature here
+                          });
